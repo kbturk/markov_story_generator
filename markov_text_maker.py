d92907e3a88d08c5f106ea27aeeeb4e88f1dbed2
Katherine Turk <kbturk87@gmail.com>
2021-01-10 13:29:37 -0600

    added story generator, which is more of a true markov text generator.

--- markov_text_maker.py
+++ markov_text_maker.py
@@ -4,43 +4,69 @@ from typing import Dict, List
 #MARKOV_WORDS: number of words in dictionary keys. 
 MARKOV_WORDS = 2
 
+NEW_NAMES:List[str] = ["Ray","Gatsby","Kanye","West","Kim","Bruce/Caitlyn","Jenner","Kardasian","Kris"]
+
+CHARACTER_NAMES = {'Jay':NEW_NAMES[0],'Gatsby':NEW_NAMES[1],
+                    'Tom':NEW_NAMES[2],'Buchanan':NEW_NAMES[3],
+                    'Daisy':NEW_NAMES[4],
+                    'Nick':NEW_NAMES[5], 'Carraway':NEW_NAMES[6],
+                    'Baker':NEW_NAMES[8],
+                    'Mina':NEW_NAMES[4],'Murray':NEW_NAMES[7],
+                    'Johnathan':NEW_NAMES[5],'Harker':NEW_NAMES[6],
+                    'Abraham':NEW_NAMES[2],'Van Helsing':NEW_NAMES[3],
+                    'Dracula':NEW_NAMES[1]}
+
+
+#Parse incoming text files into a large text string, removing page numbers, headings, and wrapped words.
 def file_parse(file:str) -> str:
-    text1 = ""
+    text = ""
     space = True
     with open(file,'r') as f:
         for line in f:
+            for name in CHARACTER_NAMES.keys():
+                line = line.replace(name,CHARACTER_NAMES[name])
             if line.rstrip() == "":
                 pass
             elif line.rstrip() =="Free eBooks at Planet eBook.com":
                 pass
-            elif line.rstrip() =="The Great Gatsby":
-                #print(f"top of page found. {line.rstrip()}")
+            elif (line.rstrip() =="The Great Gatsby") or (line.rstrip() == "Dracula"):
                 pass
             elif re.fullmatch(r"([0-9]*)",line.rstrip()):
-                #print(f"new page found: {line.rstrip()}")
                 pass
             elif re.fullmatch(r"chapter (\w*)",line.rstrip().lower()):
-                #print(f"new chapter found: {line.rstrip()}")
                 pass
             elif line.rstrip()[-1] == "-":
-                text1 += " "+line.rstrip()[:len(line.rstrip()) -1]
-                #print(f'line ended in "-": {line}')
+                text += " "+line.rstrip()[:len(line.rstrip()) -1]
                 space = False
             else:
                 if space:
-                    text1 +=" "+line.rstrip()
+                    text +=" "+line.rstrip()
                 else:
-                    text1 += line.rstrip()
+                    text += line.rstrip()
                     space = True
-    return text1
 
-def markov_builder(text2) -> Dict[str,List[str]]:
+    return text
+
+#create a markov chain dictionary using a text file.
+def markov_builder(text:str) -> Dict[str,List[str]]:
     markov_dict: Dict[str,List[str]] = {}
-    sentences = text2.split(".")
+    sentences_raw = text.split(".")
+    sentences = []
+    skip_next = False
+    #fix breaks that happen after Mr. Mrs., etc.
+    for i in range(len(sentences_raw)):
+        if sentences_raw[i][-3:-1].strip() in ["Mr","Mrs","Dr"]:
+            string = sentences_raw[i]+". "+sentences_raw[i+1]
+            skip_next = True
+        else:
+            if skip_next:
+                pass
+            else:
+                sentences.append(sentences_raw[i])
     for sent in sentences:
         words = sent.split()
         i = 0
-        if (len(words)) <= 1:
+        if (len(words)) <= (MARKOV_WORDS - 1):
             pass
         else:
             #Choose a random words.
@@ -53,7 +79,7 @@ def markov_builder(text2) -> Dict[str,List[str]]:
         
     return markov_dict
 
-def main(args) -> int:
+def main(args:List[str]) -> int:
 
     text1 = file_parse(args[1])
     text2 = file_parse(args[2])
@@ -75,7 +101,7 @@ def main(args) -> int:
                 i += len(check)
 
             if len(sent_changes) > 0:
-                next_great_americian_novel.append("*** "+random.choice(sent_changes))
+                next_great_americian_novel.append(" *** "+random.choice(sent_changes))
                 #print(f'chose:\n {next_great_americian_novel[-1]}\n')
             else:
                 next_great_americian_novel.append(sentence +".")
